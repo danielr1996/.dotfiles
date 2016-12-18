793f5c79a2a98ce8e91b94b20aee7ab7d442e75d
Daniel Richter <danielr1996@yahoo.de>
2016-12-18 20:30:35 +0100

    Refactored git prompt

--- .bashrc.d/git.mod.sh
+++ .bashrc.d/git.mod.sh
@@ -1,3 +1,13 @@
 function git_prompt(){
-	echo -e "$(color purple)*[$(color green)↑1$(color red)↓1 $(color purple)# $(color green)*$(color yellow)*$(color red)* $(color cyan)master$(color purple)]$(color)"
+	local AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+	local BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l |tr -d ' ')"
+	echo -n "$(color purple)*["
+	echo -n "$(color green)↑"$AHEAD"A"
+	echo -n "$(color red)↓"$BEHIND"B "
+	echo -n "$(color purple)# "
+	echo -n "$(color green)*"
+	echo -n "$(color yellow)*"
+	echo -n "$(color red)* "
+	echo -n "$(color cyan)master"
+	echo -n "$(color purple)]$(color)"
 }
