75d0cab57f8b6a5553992e47d03c8fa04239c58a
Daniel Richter <danielr1996@yahoo.de>
2016-12-20 02:16:36 +0100

    Update git.mod.sh

--- .bashrc.d/git.mod.sh
+++ .bashrc.d/git.mod.sh
@@ -5,7 +5,7 @@ function git_prompt(){
 	local PREFIX="$(color white)±[";
 	local AHEAD="$(color green)↑";
 	local BEHIND="$(color red)↓";
-	local MERGE="$(color purple)#֍";
+	local MERGE="$(color purple)♯";
 	local STAGED="$(color green)●";
 	local MODIFIED="$(color yellow)●";
 	local UNTRACKED="$(color red)●";
