f8f9d9190522cecd6453b7941fdfa3a943138cc7
Daniel Richter <danielr1996@yahoo.de>
2016-12-20 02:13:08 +0100

    Leerzeichen

--- .bashrc.d/prompt.mod.sh
+++ .bashrc.d/prompt.mod.sh
@@ -2,7 +2,7 @@ function bash_prompt(){
       PS1="$(color red)\u$(color)"
       PS1=$PS1"@"
       PS1=$PS1"$(color green)\h$(color)"
-      PS1=$PS1":"
+      PS1=$PS1": "
       PS1=$PS1"$(color cyan)\W "
       PS1=$PS1"$(git_prompt)"
       PS1=$PS1"\n$(color purple)> $(color)"
