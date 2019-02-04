a6c0bd501789e9a0201634ded0913a76273110ac
RichterD042 <Daniel.Richter4@arbeitsagentur.de>
2019-02-04 08:00:25 +0100

    feat: display upstream and origin changes

--- .bashrc.d/git.mod.sh
+++ .bashrc.d/git.mod.sh
@@ -3,19 +3,23 @@ function git_prompt(){
 	# Set Symbols to do display
 	##ä
 	local PREFIX="$(color purple)±[";
-	local AHEAD="$(color green)↑";
-	local BEHIND="$(color red)↓";
+	local AHEAD="$(color lightgrey)↑";
+	local BEHIND="$(color lightgrey)↓";
 	local MERGE="$(color red)Z";
 	local STAGED="$(color green)●";
 	local MODIFIED="$(color yellow)●";
 	local UNTRACKED="$(color red)●";
 	local SUFFIX="$(color purple)]";
+	local UPSTREAMAHEAD="$(color green)↑"
+	local UPSTREAMBEHIND="$(color red)↓"
 	
 	###
 	# Determinte AHEAD/BEHIND commits and branch name
 	##
 	local NAHEAD="$(git log --oneline @{push}.. 2> /dev/null | wc -l | tr -d ' ')"
 	local NBEHIND="$(git log --oneline ..@{push} 2> /dev/null | wc -l |tr -d ' ')"
+	local NUPSTREAMAHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+	local NUPSTREAMBEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')" 
 	local BRANCH="$((git symbolic-ref -q --short HEAD || git describe --tags --exact-match || git rev-parse --short HEAD)2>/dev/null)"
 	
 	# If BRANCH is empty (i.e. we are not in a git repo)
@@ -29,6 +33,16 @@ function git_prompt(){
 		# Print Prefix
 		echo -n $PREFIX
 		
+
+		if [ "$NUPSTREAMAHEAD" -gt 0 ]; then
+			echo -n $UPSTREAMAHEAD$NUPSTREAMAHEAD" "
+		fi
+		
+		if [ "$NUPSTREAMBEHIND" -gt 0 ]; then
+			echo -n $UPSTREAMBEHIND$NUPSTREAMBEHIND" "
+		fi
+
+
 		# If you are commits behind, display how many	
 		if [ "$NAHEAD" -gt 0 ]; then
 			echo -n $AHEAD$NAHEAD" "
